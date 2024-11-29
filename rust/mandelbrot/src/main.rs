705a0589497d575d84b92285302aec16a9e792ac
OTakumi <takumi.o.0410@gmail.com>
2024-11-29 21:58:37 +0900

    update

---
+++ rust/mandelbrot/src/main.rs
@@ -0,0 +1,159 @@
+use num::Complex;
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+
+        z = z * z + c;
+    }
+
+    None
+}
+
+use std::str::FromStr;
+
+/// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`
+/// Specifically, 's' shoukd have the form <left><sep><right>,
+/// where <sep> is the character given by the 'separator' argument,
+/// and <left> and <right> are both strings that can be parsed by T::from_str 'separator' must be an ASCII character.
+/// If 's' has the proper from, return 'Some<(x, y)>'.
+/// If it doesn't parse correctly, return 'None'.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None,
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+/// Parse a pair of floating-point numbers separated by a comma as a complex number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    parse_pair(s, ',').map(|(re, im)| Complex { re, im })
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+/// Given the row and column of a pixel in the output image,
+/// return the corresponding point on the complez plane.
+/// 'bounds' is a pair giving the width and height of the image in pixels.
+/// 'pixel' is a (column, row) pair indicating a particular pixel in that image.
+/// The 'upper_left' and 'lower_right' parameters are points on the complex
+/// plane designating the area our image covers.
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex {
+            re: -0.5,
+            im: -0.75
+        }
+    );
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+/// The 'bounds' argument gives the width and height of the buffer 'pixels',
+/// which holds one grayscale pixel per byte. The 'upper_left' and 'lower_right'
+/// arguments specify points on the complex plane corresponding to the upper-left
+/// and lower-right corners of the pixel buffer.
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
+
+    for r in 0..bounds.1 {
+        for c in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (c, r), upper_left, lower_right);
+            pixels[r * bounds.0 + c] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
+use image::codecs::png::PngEncoder;
+use image::ImageEncoder;
+use std::fs::File;
+
+/// Write the buffer 'pixels', whose dimensions are given by 'bounds', to the file named
+/// 'filename'.
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = match File::create(filename) {
+        Ok(f) => f,
+        Err(e) => return Err(e),
+    };
+
+    let encoder = PngEncoder::new(output);
+    encoder
+        .write_image(
+            pixels,
+            bounds.0 as u32,
+            bounds.1 as u32,
+            image::ColorType::L8.into(),
+        )
+        .unwrap();
+
+    Ok(())
+}
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+}
