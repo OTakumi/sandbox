d4363c2dd51572f37eab18ad7e04a35891e95fa3
OTakumi <takumi.o.0410@gmail.com>
2024-11-29 21:51:32 +0900

    modified: src/main.rs メイン関数を修正

--- rust/echo/src/main.rs
+++ rust/echo/src/main.rs
@@ -4,21 +4,26 @@ use clap::Parser;
 /// version: 0.1.0
 #[derive(Parser, Debug)]
 #[command(version, about)]
+#[group(required = true)]
 struct Cli {
     /// Input text
     text: Vec<String>,
 
-    /// Do not print newline
+    /// Do not output the trailing newline
     #[arg(short = 'n')]
     omit_newline: bool,
 }
 
 fn main() {
+    // let cli = Cli::parse();
     let cli = Cli::parse();
 
     // Check if omit_newline is true
     // If true, set ending to empty string
-    let ending = if cli.omit_newline { "" } else { "\n" };
-
-    print!("{}{}", cli.text.join(" "), ending);
+    // textの1つ目が-nだった場合、omit_newlineをtrueにする
+    print!(
+        "{}{}",
+        cli.text.join(" "),
+        if cli.omit_newline { "" } else { "\n" }
+    );
 }
